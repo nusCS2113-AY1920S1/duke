13bf2aea189814a22980ee5a254038ccd6c904a9
shamussy <e0310778@u.nus.edu>
2019-08-17 15:58:15 +0800

    This fixes #1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public String getGreeting() {
+        return "Hello! I'm Duke \nWhat can I do for you?";
+    }
+
+    public void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            else if (!input.isEmpty() && !input.isBlank()) {
+                System.out.println(input);
+            }
+            else {
+                System.out.println("Please do not leave any blanks");
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        System.out.println(duke.getGreeting());
+        duke.getInput();
     }
 }
