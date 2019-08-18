4282620927654a76f7fa57d540e8664dc1f0a791
e0318465 <e0318465@u.nus.edu>
2019-08-18 18:52:06 +0800

    Level 2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
+        ArrayList<String> array = new ArrayList<>();
+
         String input;
         Scanner scan = new Scanner( System.in );
         input = scan.nextLine();
+        int counter = 0;
         while(!input.equals("bye")){
-            System.out.println(input);
+            if(input.equals("list")){
+                for(int i=0; i<array.size(); i++)
+                    System.out.println((i+1) + ". " + array.get(i));
+            }
+            else{
+                array.add(counter, input);
+                counter++;
+                System.out.println("added: " + input);
+            }
             input = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
