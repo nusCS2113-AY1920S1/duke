9ee3f9d90a9a1a0a02b4dfe1464999763a8db9cf
Amos Chan <amoszzchan@outlook.com>
2019-08-18 22:38:11 +0800

    Revert "Level-1 Greet, Echo, Exit"
    This reverts commit e466ee9abc63670deeecbe9f1d91c3c4a6e6524b.
    
    Revert to original

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,31 +5,6 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         System.out.println("Hello from\n" + logo);
-
-        ArrayList<Task> list = new ArrayList<>();
-
-        Scanner scanner = new Scanner(System.in);
-
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            String[] splitStr = input.split(" ");
-            if (input.equals("list")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.print(i + 1);
-                    System.out.println(". " + "[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
-                }
-            } else if (splitStr[0].equals("done")) {
-                int n = Integer.parseInt(splitStr[1]);
-                list.get(n - 1).completeTask();
-            } else {
-                Task t = new Task(input);
-                list.add(t);
-                System.out.println("added: " + input);
-            }
-            input = scanner.nextLine();
-        }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
