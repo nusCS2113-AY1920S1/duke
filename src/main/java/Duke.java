31c7cdb6d394c2b72a42e86d74ce32d121acfd6d
e0318465 <e0318465@u.nus.edu>
2019-08-20 11:35:01 +0800

    Level 3 Completed but using own method, need to improve to the Task method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,20 +14,36 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         ArrayList<String> array = new ArrayList<>();
+        ArrayList<String> checkbox = new ArrayList<>();
 
         String input;
         Scanner scan = new Scanner( System.in );
         input = scan.nextLine();
         int counter = 0;
         while(!input.equals("bye")){
+
+            //List all saved contents in array
             if(input.equals("list")){
                 for(int i=0; i<array.size(); i++)
-                    System.out.println((i+1) + ". " + array.get(i));
+                    System.out.println((i+1) + ".[" + checkbox.get(i) + "] " + array.get(i));
             }
+
+            //Check for whether user wants to mark as done or add more input
             else{
-                array.add(counter, input);
-                counter++;
-                System.out.println("added: " + input);
+                if(input.length()>4 && input.substring(0,4).equals("done")){
+                    String digit = input.replace("done ", "");
+                    int num = Integer.parseInt(digit);
+                    checkbox.set(num-1, "\u2713");
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("[\u2713] " + array.get(num-1));
+
+                }
+                else{
+                    array.add(counter, input);
+                    checkbox.add(counter, "\u2718");
+                    counter++;
+                    System.out.println("added: " + input);
+                }
             }
             input = scan.nextLine();
         }
