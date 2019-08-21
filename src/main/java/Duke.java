ef902db1a9310562f0a3486093b6765bc01202f4
Mudaafi <SleepyUndergrad@gmail.com>
2019-08-21 18:57:23 +0800

    Duke will now echo the input provided by the user and only exit when the user types the exact string "bye".
    Abstracted the main code into an initialise() method. Declared "LOGO" and "LINE" as class variables as they may be called in other methods.
    
    Signed-off-by: Mudaafi <SleepyUndergrad@gmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String LOGO =
+            " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private final static String LINE = "________________________________________________";
+    private final static String STRING_VAR_HOLDER = "coded_start";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        initialise();
+    }
+
+    private static void initialise() {
+        // Welcome Message
+        System.out.println("Hello from the other sideeeeeeeeeeee\n" + LOGO); // Logo
+        System.out.println("Duke: Hello! I'm Duke.\nDuke: What can I do for you?");
+        System.out.println(LINE);
+        // Begin taking in input
+        Scanner scanner = new Scanner(System.in);
+        String user_input;
+        while (true) {
+            System.out.print("User: ");
+            user_input = scanner.nextLine();
+            if (user_input.equals("bye")) { // Only exit if user sends "bye" in lowercase
+                System.out.println("Duke: Bye. Hope to see you again soon!");
+                break;
+            } else { // Echo input
+                System.out.println("Duke: " + user_input +"\n");
+            }
+        }
+
+        System.out.println(LINE);
+
+
     }
 }
