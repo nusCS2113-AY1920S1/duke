8e479e8a68c5c6d0fb77c8497dc18442da2be463
GY <e0310259@u.nus.edu>
2019-08-23 20:57:19 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 
 
@@ -8,6 +9,8 @@ public class Duke {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Vector v = new Vector();
+
         System.out.println("____________________________________________________________\n"
         + " Hello! I'm Duke\n" + " What can I do for you?\n" + "____________________________________________________________\n");
 
@@ -15,8 +18,22 @@ public class Duke {
 
         while (!typing.equals("bye"))
         {
-            System.out.println("____________________________________________________________\n" +
-             typing + "\n" + "____________________________________________________________\n");
+            if (!typing.equals("list"))
+            {
+                v.add(typing);
+                System.out.println("____________________________________________________________\n" +
+                        "added: " + typing + "\n" + "____________________________________________________________\n");
+            }
+
+            else
+            {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < v.size(); i += 1)
+                {
+                    System.out.println((i + 1) + ". " + v.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            }
             typing = input.nextLine();
         }
 
