6efe6f7b9c9eb43801ad913260e0baf7cd764b1e
GY <e0310259@u.nus.edu>
2019-08-25 15:35:05 +0800

    Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,32 +9,65 @@ public class Duke {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Vector v = new Vector();
+        String line = "____________________________________________________________\n";
+        Vector<String> v = new Vector<>();
+        int[] checkbox = new int[100];
 
-        System.out.println("____________________________________________________________\n"
-        + " Hello! I'm Duke\n" + " What can I do for you?\n" + "____________________________________________________________\n");
+        System.out.println(line + " Hello! I'm Duke\n" + " What can I do for you?\n" + line);
 
         String typing = input.nextLine();
-
-        while (!typing.equals("bye"))
+        String[] splitarray = typing.split(" ", 2);
+        String first = splitarray[0];
+        String second = "0";
+        if (splitarray.length == 2)
         {
-            if (!typing.equals("list"))
+            second = splitarray[1];
+        }
+
+        while (!first.equals("bye"))
+        {
+            if (first.equals("done"))
+            {
+                int temp = Integer.parseInt(second);
+                System.out.println(temp);
+                System.out.print(line);
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[✓] " + v.get(temp - 1) + "\n" + line);
+                checkbox[temp - 1] = 1;
+            }
+
+            else if (!first.equals("list"))
             {
                 v.add(typing);
-                System.out.println("____________________________________________________________\n" +
-                        "added: " + typing + "\n" + "____________________________________________________________\n");
+                System.out.println(line + "added: " + typing + "\n" + line);
             }
 
             else
             {
-                System.out.println("____________________________________________________________");
+                System.out.print(line);
                 for (int i = 0; i < v.size(); i += 1)
                 {
-                    System.out.println((i + 1) + ". " + v.get(i));
+                    System.out.print((i + 1) + ". ");
+                    if (checkbox[i] != 1)
+                    {
+                        System.out.print("[✗] ");
+                    }
+                    else
+                    {
+                        System.out.print("[✓] ");
+                    }
+                    System.out.println(v.get(i));
                 }
-                System.out.println("____________________________________________________________");
+                System.out.print(line);
             }
+
             typing = input.nextLine();
+            splitarray = typing.split(" ", 2);
+            first = splitarray[0];
+            if (splitarray.length == 2)
+            {
+                second = splitarray[1];
+            }
         }
 
         System.out.println("____________________________________________________________\n" +
