7282d1c1df71e512ed4127720fb4c18219a56574
aarushisingh1 <singh.aarushi@outlook.com>
2019-08-26 22:39:17 +0800

    Level 2 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,22 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, I'm Duke! What can I do for you today?");
         input = scanner.nextLine();
+        String arr[] = new String[100];
+        int i = 0;
         while(!input.equals("bye")) {
-            System.out.println(input);
-            input = scanner.nextLine();
+            //System.out.println(input);
+            if(!input.equals("list")){
+                System.out.println("added: " + input);
+                arr[i] = input;
+                input = scanner.nextLine();
+                i += 1;
+            }
+            else{
+                for(int j=0;j<i;j++){
+                    System.out.println((j+1) + ". " + arr[j]);
+                }
+                input = scanner.nextLine();
+            }
         }
 
         {
