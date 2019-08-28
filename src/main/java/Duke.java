6d44d5038c377499e95599a8c830a72becc46230
Qian Jie <qianjie777@gmail.com>
2019-08-28 16:42:56 +0800

    Level-3: Improvement on code organization by introducing a new class "MyDuke" that consist of all the methods/abilities of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,4 @@
-import myduke.Task;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import myduke.MyDuke;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,32 +9,8 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        ArrayList<Task> taskBox = new ArrayList<Task>();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        while(true){
-            String description = scan.nextLine();
-            if (description.equals("bye")){
-                break;
-            }else if(description.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskBox.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskBox.get(i));
-                }
-
-            }else if(description.startsWith("done")){
-                int index = Integer.parseInt(description.split(" ")[1]);
-                Task target = taskBox.get(index - 1);
-                target.markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(taskBox.get(index - 1));
-            }else{
-                System.out.println("added: " + description);
-                taskBox.add(new Task(description));
-            }
-
-
-        }
-        System.out.println("Bye. Hope to see you again soon!");
+        MyDuke duke = new MyDuke();
+        duke.greet();
+        duke.run();
     }
 }
--- src/main/java/myduke/MyDuke.java
+++ src/main/java/myduke/MyDuke.java
@@ -1,17 +1,45 @@
 package myduke;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MyDuke {
-    private String description;
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Task> taskBox = new ArrayList<Task>();
 
     public void greet(){
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
-    public void echo(String description){
-        System.out.println("added: " + description);
+    public void echo(String inputs){
+        System.out.println("added: " + inputs);
+        taskBox.add(new Task(inputs));
     }
 
     public void exit(){
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    public void run(){
+        while(true){
+            String description = scan.nextLine();
+            if (description.equals("bye")){
+                this.exit();
+                break;
+            }else if(description.equals("list")){
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskBox.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskBox.get(i));
+                }
+
+            }else if(description.startsWith("done")){
+                int index = Integer.parseInt(description.split(" ")[1]);
+                Task target = taskBox.get(index - 1);
+                target.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(taskBox.get(index - 1));
+            }else{
+                this.echo(description);
+            }
+        }
+    }
 }
