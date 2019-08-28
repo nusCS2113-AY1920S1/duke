f578f6da3ac181cd68690ca7d9167df1f403b4c7
Justin Chia <justinchiajj@hotmail.com>
2019-08-28 20:38:25 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        while(true){
+            String input = scan.nextLine();
+            if (input.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            else{
+                System.out.println(input + "\n");
+            }
+        }
     }
 }
