93b583915e6aa04d38c0772e69ced65b0bfb20b1
Justin Chia <justinchiajj@hotmail.com>
2019-08-28 22:54:38 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,65 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         while(scan.hasNext()){
             String input = scan.nextLine();
-            if (input.equals("bye")){
+            String command = input.split(" ")[0];
+            switch(command){
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.exit(0);
+
+                case "list":
+                    System.out.println("Here are the tasks in your list: ");
+                    for(int i=0; i < taskList.size(); i+=1){
+                        System.out.println(i+1 + "." + taskList.get(i).toString());
+                    }
+                    break;
+                case "done":
+                    int taskNumber = Integer.parseInt(input.split(" ")[1]) -1;
+                    taskList.get(taskNumber).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + taskList.get(taskNumber).toString());
+                    break;
+                case "todo" :
+                    String description = input.substring(4).trim();
+                    ToDo newToDo = new ToDo(description);
+                    taskList.add(newToDo);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + newToDo.toString());
+                    System.out.println("Now you have " + taskList.size() + " tasks in the list");
+                    break;
+                case "deadline":
+                    String[] deadlineInfo = input.substring(8).split(" /by ");
+                    Deadline newDeadline = new Deadline(deadlineInfo[0], deadlineInfo[1]);
+                    taskList.add(newDeadline);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + newDeadline.toString());
+                    System.out.println("Now you have " + taskList.size() + " tasks in the list");
+                    break;
+                case "event":
+                    String[] eventInfo = input.substring(5).split(" /at ");
+                    Events newEvent = new Events(eventInfo[0], eventInfo[1]);
+                    taskList.add(newEvent);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + newEvent.toString());
+                    System.out.println("Now you have " + taskList.size() + " tasks in the list");
+                    break;
+
+                default:
+                    Task newTask = new Task(input);
+                    taskList.add(newTask);
+                    System.out.println("added: " + input);
+
+            }
+
+
+
+            /*
+            if (command.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 System.exit(0);
             }
 
-            else if(input.equals("list")){
+            else if(command.equals("list")){
                 for(int i=0; i < taskList.size(); i+=1){
                     System.out.println("Here are the tasks in your list: ");
                     System.out.println(i+1 + "." + taskList.get(i).toString());
@@ -38,6 +91,7 @@ public class Duke {
                 taskList.add(newTask);
                 System.out.println("added: " + input);
             }
+             */
         }
     }
 }
