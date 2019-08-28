c5cbfd67a24b081e0e514861fd051a5e96c58e14
KaneQuah <kane_quah@u.nus.edu>
2019-08-29 04:00:51 +0800

    Level 1 Greet, Echo, Exit
    Signed-off-by: KaneQuah <kane_quah@u.nus.edu>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        String input = new String();
+        while(sc.hasNextLine() && !input.equalsIgnoreCase("bye"))
+        {
+            input = sc.nextLine();
+            if(input.equalsIgnoreCase("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+            else {
+                System.out.println(input);
+            }
+        }
+
+        sc.close();
     }
 }
