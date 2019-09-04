fb3e8fbd6696dddfd4c4eb065c88e20c6c0563d7
Qian Jie <qianjie777@gmail.com>
2019-09-04 16:17:37 +0800

    A-TextUiTesting: Text UI Testing [Completed]

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -18,19 +18,19 @@ public class Ui {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        showLine();
-        System.out.println("Hello from\n" + logo);
+        //showLine();
+        //System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        showLine();
+        //showLine();
     }
 
     public void showTaskList(ArrayList<Task> taskList) {
-        showLine();
+        //showLine();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); i++) {
             System.out.println((i + 1) + ". " + taskList.get(i));
         }
-        showLine();
+        //showLine();
     }
 
     public void showExit() {
@@ -38,55 +38,55 @@ public class Ui {
     }
 
     public void showDone(ArrayList<Task> taskList , int index) {
-        showLine();
+        //showLine();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(taskList.get(index - 1));
-        showLine();
+        //showLine();
     }
 
     public void showDelete(ArrayList<Task> taskList , int index) {
-        showLine();
+        //showLine();
         System.out.println("Noted. I've removed this task:");
         System.out.println(taskList.get(index - 1));
     }
 
     public void showTaskListSize(ArrayList<Task> taskList) {
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-        showLine();
+        //showLine();
     }
 
     public void showMatchTasks(ArrayList<Task> searchResults , int index) {
-        showLine();
+        //showLine();
         System.out.println("Here are the matching tasks in your list:");
         for (Task results : searchResults) {
             index++;
             System.out.println(index + "." + results);
         }
-        showLine();
+        //showLine();
     }
 
     public void showAddTodoTask(ArrayList<Task> taskList , Task todoTask) {
-        showLine();
+        //showLine();
         System.out.println("Got it. I've added this task:");
         System.out.println(todoTask);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-        showLine();
+        //showLine();
     }
 
     public void showAddDeadlineTask(ArrayList<Task> taskList , Task deadlineTask) {
-        showLine();
+        //showLine();
         System.out.println("Got it. I've added this task:");
         System.out.println(deadlineTask);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-        showLine();
+        //showLine();
     }
 
     public void showAddEventTask(ArrayList<Task> taskList , Task eventTask) {
-        showLine();
+        //showLine();
         System.out.println("Got it. I've added this task:");
         System.out.println(eventTask);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-        showLine();
+        //showLine();
     }
 
     public void showError (String message) {
